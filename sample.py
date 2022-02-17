5960595b7982b4da8ab67e50af72f06b1d779228
vikas99341 <vikasisinlove@gmail.com>
2022-02-17 01:26:23 +0000

    new branch added

--- sample.py
+++ sample.py
@@ -3,3 +3,4 @@
 print (" Learning git merge and new concepts added " )
 print (" new line added ")
 print ("fourth line added and will add fifth line too ")
+print (" New line added on dev branch ")
