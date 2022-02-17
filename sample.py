1be7bf23ac8d2e8767ceeb8f323d9ef54133ec0b
vikas99341 <48900898+vikas99341@users.noreply.github.com>
2022-02-17 06:42:11 +0530

    updated sample.py

--- sample.py
+++ sample.py
@@ -1,5 +1,5 @@
 #this is a dummy line 
 
-print (" Happy learning new concepts added ")
+print (" new concepts added ")
 print (" new line added ")
 print ("fourth line added and will add fifth line too ")
