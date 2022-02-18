dcef116a3ce9689363982e04a4078d675daf9782
vikas99341 <vikasisinlove@gmail.com>
2022-02-18 01:31:13 +0000

    using git revert
    This reverts commit ae3d218e141d832be6301e0e70a2bb456ce77ebd.

--- sample.py
+++ sample.py
@@ -1,5 +1,5 @@
 #this is a dummy line 
 
-print (" Learning git merge ")
+print (" Happy learning new concepts added ")
 print (" new line added ")
 print ("fourth line added and will add fifth line too ")
